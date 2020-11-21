38417336e18e0560e73f255b8505c4e8b7c1a7e5
Michael Perry <mperry777@gmail.com>
2020-11-21 02:08:17 -0500

    Implemented TestSubtract

--- calculator.go
+++ calculator.go
@@ -3,11 +3,19 @@ package calculator
 
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
-return a + b
+	return a + b
 }
 
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
 func Subtract(a, b float64) float64 {
-	return b - a
+	if a < b {
+		return b - a
+	}
+	return a - b
+}
+
+// Multiply takes two numbers and returns the multiplied result
+func Multiply(a, b float64) float64 {
+	return a * b
 }
--- calculator_test.go
+++ calculator_test.go
@@ -14,11 +14,20 @@ func TestAdd(t *testing.T) {
 	}
 }
 
-// func TestSubtract(t *testing.T) {
-// 	t.Parallel()
-// 	var want float64 = 2
-// 	got := calculator.Subtract(4, 2)
-// 	if want != got {
-// 		t.Errorf("want %f, got %f", want, got)
-// 	}
-// }
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	var want float64 = 2
+	got := calculator.Subtract(4, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var want float64 = 10
+	got := calculator.Multiply(2, 5)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
