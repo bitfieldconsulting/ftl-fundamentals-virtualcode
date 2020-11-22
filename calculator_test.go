d79b3d764d6903b695c6070204df7e27d1663b02
Michael Perry <mperry777@gmail.com>
2020-11-22 12:47:26 -0500

    TestAdd uses struct to test for additional checks

--- calculator_test.go
+++ calculator_test.go
@@ -7,10 +7,20 @@ import (
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 1, want: 2},
+		{a: 5, b: 0, want: 5},
+	}
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
 
@@ -29,5 +39,27 @@ func TestMultiply(t *testing.T) {
 	got := calculator.Multiply(2, 5)
 	if want != got {
 		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		a, b        float64
+		want        float64
+		errExpected bool
+	}
+	testCases := []testCase{
+		{a: 1, b: 1, want: 1, errExpected: false},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Divide(tc.a, tc.b)
+		errReceived := err != nil
+		if errReceived != tc.errExpected {
+			t.Fatalf("Divide(%f, %f): unexpected error status: %v", tc.a, tc.b, err)
+		}
+		if !tc.errExpected && tc.want != got {
+			t.Errorf("Divide(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
